1054173b55b1ecc7875c44bd0a614203ce05fa9e
Sehgal, Amit <asehgal@verisign.com>
2015-06-10 13:03:04 -0400

    added region the repo

--- src/main/java/org/wikimedia/elasticsearch/swift/repositories/SwiftAccountFactory.java
+++ src/main/java/org/wikimedia/elasticsearch/swift/repositories/SwiftAccountFactory.java
@@ -4,17 +4,15 @@ import org.javaswift.joss.model.Account;
 
 public class SwiftAccountFactory {
 
-    public static Account createAccount(SwiftService swiftService, String url, String username, String password, String tenantName, String authMethod) {
+    public static Account createAccount(SwiftService swiftService, String url, String username, String password, String tenantName, String authMethod, String region) {
         if ("KEYSTONE".equals(authMethod.toUpperCase())) {
-            return swiftService.swiftKeyStone(url, username, password, tenantName);
+            return swiftService.swiftKeyStone(url, username, password, tenantName, region );
         }
 
         if ("TEMPAUTH".equals(authMethod.toUpperCase())) {
             return swiftService.swiftTempAuth(url, username, password);
         }
-
         return swiftService.swiftBasic(url, username, password);
-
     }
 
 }
